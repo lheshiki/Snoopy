793140b759a4f8c4a5decb33d44fd6be67c25276
Kitano Yudai <>
2020-11-30 13:46:42 +0900

    手抜きハンバーガー

--- hb.js
+++ hb.js
@@ -1,6 +1,15 @@
-$(function () {
-    $('.navbar_toggle').on('click', function () {
-      $(this).toggleClass('open');
-      $('.hb_menu').toggleClass('open');
+// ハンバーガー
+$(function() {
+    console.log('load');
+    $('.hamburger').click(function() {
+        console.log('hamburger');
+
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
     });
-  });
+});
